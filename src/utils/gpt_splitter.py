ee2dfd0c263a2459d87bfea4f11d4a7787fcecf8
Torbjørn Opheim <torbjornop@gmail.com>
2023-06-02 00:36:02 +0200

    improve gpt_splitter

--- src/utils/gpt_splitter.py
+++ src/utils/gpt_splitter.py
@@ -1,5 +1,8 @@
-# Alternatively you can use web search and link to the transcript!
+import glob
+import os
 
+
+# Alternatively you can use web search and link to the transcript!
 def split_inputs(filename: str):
     """
     Split input text into 4000 character or 500 word chunks
@@ -10,26 +13,37 @@ def split_inputs(filename: str):
         wordcount = 0
         character_count = 0
         chunks: list[str] = []
-        last_split=0
+        last_split = 0
         for i, line in enumerate(content):
-            if (character_count + len(line)) // 4000 > 0:
-                chunks.append(content[last_split:i-1])
+            if (character_count + len(line)) // 8000 > 0:
+                chunks.append("".join(content[last_split:i-1]))
                 last_split = i-1
-                character_count=0
-                wordcount=0
-            if (wordcount + len(line.split(" "))) // 500 > 0:
-                chunks.append(content[last_split:i-1])
-                last_split = i-1
-                character_count=0
-                wordcount=0
-            character_count+= len(line)
-            wordcount+= len(line.split(" "))
+                character_count = 0
+                wordcount = 0
+            # if (wordcount + len(line.split(" "))) // 500 > 0:
+            #     chunks.append("".join(content[last_split:i-1]))
+            #     last_split = i-1
+            #     character_count = 0
+            #     wordcount = 0
+            character_count += len(line)
+            wordcount += len(line.split(" "))
 
     for i, chunk in enumerate(chunks):
         with open(filename.split(".")[0] + "_" + str(i) + ".txt", "w") as outfile:
             if chunk:
-                outfile.write("\n".join(chunk))
+                outfile.write(chunk)
 
 
-split_inputs(filename="transcript.txt")
-print()
+
+# Define the path where you want to search for transcript.txt files
+
+search_path = os.getcwd()
+# Use glob to find all files called transcript.txt in the search path
+file_paths = glob.glob(search_path + "/**/transcript.txt", recursive=True)
+
+# Print the file paths
+for path in file_paths:
+    print(path)
+    input("continue?")
+    split_inputs(filename=path)
+
